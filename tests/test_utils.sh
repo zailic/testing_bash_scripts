5f1ae089db284d6ad0602d305474b5f308348795
Ionut Ovidiu Zailic <ionut@zailic.com>
2019-03-20 12:26:22 +0200

    Create test_utils.sh

---
+++ tests/test_utils.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+# shellcheck disable=SC1091
+source ./../includes/utils.sh
+
+test_superuser_required() {
+    assert_fail run_as_superuser_trap "Are you running these tests as root? You shouldn't:)"
+}
+
+test_should_get_error_message() {
+    assert_equals "Error! Root privileges are required in order to run this script." "$(run_as_superuser_trap 2>&1)" \
+        "Are you running these tests as root? You shouldn't:)"
+}
