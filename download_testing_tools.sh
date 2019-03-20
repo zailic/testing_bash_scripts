2b10fca54a8a5bf21f6e877edcf2c28aeca0d24b
Ionut Ovidiu Zailic <ionut@zailic.com>
2019-03-20 12:40:35 +0200

    Update download_testing_tools.sh

--- download_testing_tools.sh
+++ download_testing_tools.sh
@@ -21,13 +21,16 @@ function download() {
     if [[ ! -f "${__tools_dir}/shellcheck" ]]; then
         curl https://storage.googleapis.com/shellcheck/shellcheck-stable.linux.x86_64.tar.xz | \
             tar -xJv --strip-components=1 -C "${__tools_dir}" shellcheck-stable/shellcheck
+        chmod +x "${__tools_dir}/shellcheck"
     fi
     # bash unit
     if [[ ! -f "${__tools_dir}/bash_unit" ]]; then
         __pushd "${__tools_dir}"
         bash <(curl -s https://raw.githubusercontent.com/pgrange/bash_unit/master/install.sh)
         __popd
+        chmod +x "${__tools_dir}/shellcheck"
     fi
+    
 }
 
 download
